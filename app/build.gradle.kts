95e449e683f58fcff8a0ee9e684ad0350532596a
Pavel <kulikov-pavlik@bk.ru>
2023-10-11 20:40:42 +0300

    Created started visual navigation bar. Without navigation yet.

---
+++ app/build.gradle.kts
@@ -0,0 +1,126 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+    id("com.google.dagger.hilt.android")
+    id("dagger.hilt.android.plugin")
+    id("kotlin-kapt")
+}
+
+android {
+    namespace = "com.crystal2033.qrextractor"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.crystal2033.qrextractor"
+        minSdk = 24
+        targetSdk = 33
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        vectorDrawables {
+            useSupportLibrary = true
+        }
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+    kotlinOptions {
+        jvmTarget = "17"
+        allWarningsAsErrors = false
+        freeCompilerArgs += listOf(
+            "-opt-in=androidx.compose.material3.ExperimentalMaterial3Api"
+        )
+    }
+    buildFeatures {
+        compose = true
+    }
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.4.3"
+    }
+    packaging {
+        resources {
+            excludes += "/META-INF/{AL2.0,LGPL2.1}"
+        }
+    }
+}
+
+dependencies {
+
+    val hilt_version = "2.44"
+    implementation("com.google.dagger:hilt-android:$hilt_version")
+    kapt("com.google.dagger:hilt-android-compiler:$hilt_version")
+    kapt("androidx.hilt:hilt-compiler:1.0.0")
+
+    // https://mvnrepository.com/artifact/androidx.hilt/hilt-navigation-compose
+    implementation("androidx.hilt:hilt-navigation-compose:1.0.0")
+
+    //Coroutines
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
+
+    val nav_version = "2.7.4"
+    // Jetpack Compose Integration
+    implementation("androidx.navigation:navigation-compose:$nav_version")
+
+    // Optional - Integration with ViewModels
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.6.2")
+
+    //For viewmodel
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.2")
+
+    //Retrofit
+    val retrofitVersion = "2.9.0"
+    implementation ("com.squareup.retrofit2:retrofit:${retrofitVersion}")
+    implementation ("com.squareup.retrofit2:converter-gson:${retrofitVersion}")
+
+    // https://mvnrepository.com/artifact/com.squareup.okhttp3/okhttp
+    implementation("com.squareup.okhttp3:okhttp:4.11.0")
+
+    // CameraX core library using the camera2 implementation
+    val camerax_version = "1.4.0-alpha01"
+    implementation("androidx.camera:camera-camera2:${camerax_version}")
+    // If you want to additionally use the CameraX Lifecycle library
+    implementation("androidx.camera:camera-lifecycle:${camerax_version}")
+//    // If you want to additionally use the CameraX VideoCapture library
+//    implementation("androidx.camera:camera-video:${camerax_version}")
+    // If you want to additionally use the CameraX View class
+    implementation("androidx.camera:camera-view:${camerax_version}")
+
+    //QR-code
+    implementation("com.google.zxing:core:3.4.0")
+
+//    val room_version = "2.5.2"
+//    implementation("androidx.room:room-runtime:$room_version")
+//    kapt("androidx.room:room-compiler:$room_version")
+//    implementation("androidx.room:room-ktx:2.5.2")
+//    // optional - Kotlin Extensions and Coroutines support for Room
+//    implementation("androidx.room:room-ktx:$room_version")
+//    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
+
+    implementation("androidx.core:core-ktx:1.9.0")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
+    implementation("androidx.activity:activity-compose:1.8.0")
+    implementation(platform("androidx.compose:compose-bom:2023.03.00"))
+    implementation("androidx.compose.ui:ui")
+    implementation("androidx.compose.ui:ui-graphics")
+    implementation("androidx.compose.ui:ui-tooling-preview")
+    implementation("androidx.compose.material3:material3")
+    testImplementation("junit:junit:4.13.2")
+    androidTestImplementation("androidx.test.ext:junit:1.1.5")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    androidTestImplementation(platform("androidx.compose:compose-bom:2023.03.00"))
+    androidTestImplementation("androidx.compose.ui:ui-test-junit4")
+    debugImplementation("androidx.compose.ui:ui-tooling")
+    debugImplementation("androidx.compose.ui:ui-test-manifest")
+}
---
+++ app/src/main/java/com/crystal2033/qrextractor/MainActivity.kt
@@ -0,0 +1,98 @@
+package com.crystal2033.qrextractor
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AddCircle
+import androidx.compose.material.icons.outlined.AddCircle
+import androidx.compose.material3.Icon
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.vectorResource
+import androidx.compose.ui.unit.dp
+import com.crystal2033.qrextractor.ui.theme.QRExtractorTheme
+
+data class BottomNavigationItem(
+    val title: String,
+    val selectedIcon: ImageVector,
+    val unselectedIcon: ImageVector
+)
+
+//object MyAppIcons {
+//    val historyQRCodesIcon = R.drawable.filled_work_history_24
+//    val addQRCodeIcon =
+//}
+
+class MainActivity : ComponentActivity() {
+    @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            QRExtractorTheme {
+                val bottomItems = listOf(
+                    BottomNavigationItem(
+                        title = "Last scanned",
+                        selectedIcon = ImageVector.vectorResource(R.drawable.filled_work_history_35),
+                        unselectedIcon = ImageVector.vectorResource(R.drawable.outline_work_history_35)
+                    ),
+                    BottomNavigationItem(
+                        title = "Add data",
+                        selectedIcon = ImageVector.vectorResource(R.drawable.filled_add_to_photos_35),
+                        unselectedIcon = ImageVector.vectorResource(R.drawable.outline_add_to_photos_35)
+                    ),
+                    BottomNavigationItem(
+                        title = "Scanner",
+                        selectedIcon = ImageVector.vectorResource(R.drawable.filled_qr_code_35),
+                        unselectedIcon = ImageVector.vectorResource(R.drawable.outline_qr_code_35)
+                    ),
+                    BottomNavigationItem(
+                        title = "Documents",
+                        selectedIcon = ImageVector.vectorResource(R.drawable.filled_description_35),
+                        unselectedIcon = ImageVector.vectorResource(R.drawable.outline_description_35)
+                    )
+                )
+                var selectedItemIndex by rememberSaveable {
+                    mutableIntStateOf(0)
+                }
+
+                Scaffold(
+                    bottomBar = {
+                        NavigationBar {
+                            bottomItems.forEachIndexed { index, bottomItem ->
+                                NavigationBarItem(
+                                    selected = selectedItemIndex == index,
+                                    onClick = {
+                                              selectedItemIndex = index
+                                    },
+                                    modifier = Modifier.size(100.dp),
+                                    icon = {
+                                        Icon(
+                                            imageVector = if (index == selectedItemIndex){
+                                                bottomItem.selectedIcon
+                                            }
+                                            else bottomItem.unselectedIcon,
+                                            contentDescription = bottomItem.title
+                                        )
+                                    })
+
+                            }
+                        }
+                    }
+                ) {
+
+                }
+            }
+        }
+    }
+}
